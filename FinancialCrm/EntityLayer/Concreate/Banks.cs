276d3c79670c62ad3e0029856e4feab0fd9d837d
ömer faruk yazıcı <omer-faruk-yazici@outlook.com>
2025-10-11 02:06:23 +0300

    Bağamlılıklar azaltılmaya çalışıldı

---
+++ FinancialCrm/EntityLayer/Concreate/Banks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialCrm.Entities
+{
+    public class Banks
+    {
+        public int BankId { get; set; }
+        public string BankTitle { get; set; }
+        public decimal BankBalance { get; set; }
+        public int BankAccountNumber { get; set; }
+
+    }
+}
---
+++ FinancialCrm/EntityLayer/Concreate/BillPeriods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialCrm.Entities
+{
+    public class BillPeriods
+    {
+        public int BillPeriodId { get; set; }
+        public string PeriodName { get; set; }
+    }
+}
